bd5bb6483d7fd731af386a42c48277c4c207f023
Faiz Rizal <muchammad.faiz.f@gmail.com>
2026-06-30 20:32:50 +0700

    perbaikan login action

--- test/actions/login.action.js
+++ test/actions/login.action.js
@@ -1,18 +1,36 @@
 import loginPage from '../pageobjects/login.page'
-import { LoginAction } from '../action/login.action'
 
-const action = new LoginAction()
+class LoginAction {
+    async enterUsername(username) {
+        const fields = await $$(loginPage.usernameInput)
+        if (fields.length >= 1) {
+            await fields[0].setValue(username)
+        }
+    }
 
-export default {
+    async enterPassword(password) {
+        const fields = await $$(loginPage.passwordInput)
+        if (fields.length >= 2) {
+            await fields[1].setValue(password)
+        }
+    }
+
+    async tapLogin() {
+        const btn = await $(loginPage.loginButton)
+        await btn.click()
+    }
+
     async isOnLoginPage() {
         const btn = await $(loginPage.loginButton)
         await btn.waitForDisplayed({ timeout: 15000 })
         return btn.isDisplayed()
-    },
+    }
 
     async login(username, password) {
-        await action.login(username, password)
-    },
+        await this.enterUsername(username)
+        await this.enterPassword(password)
+        await this.tapLogin()
+    }
 
     async waitForProducts(timeout = 5000) {
         const el = await $(loginPage.productsTitle)
@@ -20,3 +38,5 @@ export default {
         return el.isDisplayed()
     }
 }
+
+export default new LoginAction()
